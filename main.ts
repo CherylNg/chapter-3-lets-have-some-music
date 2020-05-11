997dcb7d6a353b5ac703f4451b9eda1683aff686
CherylNg <61680146+CherylNg@users.noreply.github.com>
2020-05-11 15:06:43 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 function I_Will_Follow_You () {
+    // line 1: I will follow you,
     music.playTone(262, music.beat(BeatFraction.Half))
     music.playTone(294, music.beat(BeatFraction.Half))
     music.playTone(349, music.beat(BeatFraction.Half))
     music.playTone(392, music.beat(BeatFraction.Half))
     music.playTone(349, 2.5 * music.beat(BeatFraction.Whole))
     music.rest(music.beat(BeatFraction.Half))
+    // line 2: Follow you wherever you may go
     music.playTone(262, music.beat(BeatFraction.Half))
     music.playTone(294, music.beat(BeatFraction.Half))
     music.playTone(349, music.beat(BeatFraction.Half))
@@ -15,6 +17,7 @@ function I_Will_Follow_You () {
     music.playTone(440, music.beat(BeatFraction.Half))
     music.playTone(523, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Half))
+    // line 3: There isn't an ocean too deep,
     music.playTone(523, music.beat(BeatFraction.Half))
     music.playTone(587, music.beat(BeatFraction.Half))
     music.playTone(587, music.beat(BeatFraction.Half))
@@ -24,6 +27,7 @@ function I_Will_Follow_You () {
     music.playTone(587, music.beat(BeatFraction.Half))
     music.playTone(523, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Half))
+    // line 4: A mountain so high it can keep,
     music.playTone(523, music.beat(BeatFraction.Half))
     music.playTone(587, music.beat(BeatFraction.Half))
     music.playTone(587, music.beat(BeatFraction.Half))
@@ -33,6 +37,7 @@ function I_Will_Follow_You () {
     music.playTone(494, music.beat(BeatFraction.Half))
     music.playTone(523, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Half))
+    // line 5: Keep me away...
     music.playTone(523, music.beat(BeatFraction.Half))
     music.playTone(440, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Whole))
